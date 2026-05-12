dbbb29fc26931e9ba56b596a0dc2b5c9ccbefb87
juanmarore07 <239345310+juanmarore07@users.noreply.github.com>
2026-05-12 14:08:05 +0200

    Fichero aldea.dat añadido y también nombre cambiado en las funciones

--- src/datas/Aldea.java
+++ src/datas/Aldea.java
@@ -25,12 +25,12 @@ public class Aldea {
     public void cargarFichero() {
         try {
             //Voy a abrir un canal de lectura de objetos sobre un fichero binario
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("repuestos.dat"));
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("aldea.dat"));
             setMapaAladeanos((Map<String, Aldeano>) ois.readObject());
             ois.close();
 
         } catch (FileNotFoundException e) {
-            System.out.println("El fichero repuestos.dat no existe");
+            System.out.println("El fichero aldea.dat no existe");
         } catch (ClassNotFoundException | IOException e) {
             System.out.println(e.getMessage());
         }
@@ -40,7 +40,7 @@ public class Aldea {
         //Tengo que meter los datos de este club (this) en un fichero
         try {
             //Voy a abrir un canal de escritura de objetos sobre un fichero binario
-            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("repuestos.dat"));
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("aldea.dat"));
             oos.writeObject(mapaAladeanos); //Poniendo this nos referimos a este club, este objeto
         }catch (IOException e) {
             e.printStackTrace();
